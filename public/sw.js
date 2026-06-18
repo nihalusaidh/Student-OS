1236a70a8f7bff977068bd955796651e2c605ba2
unknown <nihalusaidh07@gmail.com>
2026-06-18 09:23:50 +0530

    Fix PWA cache white screen

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,16 @@
-const CACHE_NAME = "student-os-v1";
-
-const urlsToCache = [
-  "/",
-  "/manifest.json"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.map((name) => caches.delete(name)))
+    )
   );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // Do nothing. Always let the browser/network handle requests.
 });
